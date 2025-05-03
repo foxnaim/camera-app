377fd6db1f24ab3508dca6b3c32018030ee02c16
NextFuture <nextfuture.pvl@gmail.com>
2025-05-03 17:37:32 +0500

    fix v7

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,6 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        CapacitorCommunityMediaPlugin.register(with: CapacitorBridge.shared)
         // Override point for customization after application launch.
         return true
     }
--- src/components/CameraInput.tsx
+++ src/components/CameraInput.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { Media } from "@capacitor-community/media";
+import { Capacitor } from "@capacitor/core";
+
+console.log("Capacitor version:", Capacitor.getPlatform());
+console.log("Media plugin available:", !!Media.pickImages);
 
 declare module "@capacitor-community/media" {
   export interface MediaPlugin {
@@ -12,39 +16,23 @@ declare module "@capacitor-community/media" {
 const ImageInput = () => {
   const [selectedImage, _setSelectedImage] = useState<string | null>(null);
 
-  // Функция для выбора изображения только из галереи
   const pickImageFromGallery = async () => {
     try {
       const result = await Media.pickImages({ limit: 1 });
 
       if (result && result.photos.length > 0) {
         const photo = result.photos[0];
-        console.log("Selected photo path:", photo.path); // Используй для отображения
+        console.log("Selected photo path:", photo.path);
       }
     } catch (error) {
       console.error("Ошибка при выборе изображения:", error);
     }
   };
 
-  //   const openGallery = async () => {
-  //     try {
-  //       const image = await Camera.getPhoto({
-  //         quality: 90,
-  //         source: CameraSource.Photos, // Источник - галерея
-  //         resultType: CameraResultType.Uri, // Возвращаем URI изображения
-  //       });
-
-  //       // Здесь можно обработать полученное изображение
-  //       console.log(image);
-  //     } catch (error) {
-  //       console.error("Ошибка при открытии галереи:", error);
-  //     }
-  //   };
-
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
       {/* Кнопка для открытия галереи */}
-      <button onClick={pickImageFromGallery}>Загрузить из галереи v6</button>
+      <button onClick={pickImageFromGallery}>Загрузить из галереи v7</button>
 
       {/* Если изображение выбрано, показываем его */}
       {selectedImage && (
